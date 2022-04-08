e1724ee342eeb515ea043995515c5eb89233a388
ASHWINKUMARJT <103244236+ASHWINKUMARJT@users.noreply.github.com>
2022-04-08 13:21:07 +0530

    Update python-driver-code.py

--- numbers/python-driver-code.py
+++ numbers/python-driver-code.py
@@ -1,7 +1,19 @@
 # Python Driver Code
 
 def solve(n: int) -> str:
-  # Your code goes here
+  sum=0;
+  while(n>0 or sum>9);
+  if(n==0);
+  n=sum;
+  sum=0;
+  sum=sum+n%10;
+  n=int(n/10);
+  return True if (sum==1)else False;
+n=1234;
+if(isMagic(n)):
+  print("Magic Number");
+  else:
+    print("Not a magic Number");
   # n is the given input
   return "Special"
 
